f78e656726577c84b6468ef46aa2b1052dd0cabc
Mary Wong <pair+mwong@modcloth.com>
2012-09-08 19:01:49 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ggit::Application.config.secret_token = '81c3aba274156ea7a02ff2492bddc1d8d25fdf9508584cbd4da487d7c094d99691e52ef779c48947cc37af28a759a3785ca357079a249c1ee442b7803b2b74f5'
